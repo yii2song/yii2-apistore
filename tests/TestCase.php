0914f4bee903031327d9b919ffac80293ec037c0
Xu Tongle <xutongle@gmail.com>
2016-02-01 10:32:48 +0800

    update

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -50,7 +50,7 @@ abstract class TestCase extends \PHPUnit_Framework_TestCase
             'id' => 'testapp',
             'basePath' => __DIR__,
             'vendorPath' => dirname(__DIR__) . '/vendor',
-            'services' => [
+            'components' => [
                 'request' => [
                     'cookieValidationKey' => 'wefJDF8sfdsfSDefwqdxj9oq',
                     'scriptFile' => __DIR__ . '/index.php',
