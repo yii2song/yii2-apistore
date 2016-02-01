f036d23390b4e188b392b0b887a397e35764a089
Xu Tongle <xutongle@gmail.com>
2016-02-01 10:30:26 +0800

    update

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -15,7 +15,7 @@ $_SERVER['SCRIPT_NAME'] = '/' . __DIR__;
 $_SERVER['SCRIPT_FILENAME'] = __FILE__;
 
 require_once(__DIR__ . '/../vendor/autoload.php');
-require_once(__DIR__ . '/../vendor/yiisoft/framework/Yii.php');
+require_once(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
 Leaps::setAlias('@xutongle/extensions/apistore', __DIR__);
 Leaps::setAlias('@xutongle/apistore', dirname(__DIR__));
