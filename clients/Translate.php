a4d2d5d53dee1f85ca67855ece3922c17c6670f6
Xu Tongle <xutongle@gmail.com>
2016-02-01 11:41:06 +0800

    更新百度翻译接口，原接口已经失效

--- clients/Translate.php
+++ clients/Translate.php
@@ -3,9 +3,40 @@
 namespace xutongle\apistore\clients;
 
 use xutongle\apistore\Baidu;
+use xutongle\apistore\BaseClient;
 
-class Translate extends Baidu
+/**
+ * Class Translate 实现了百度翻译的相关接口
+ * @package xutongle\apistore\clients
+ */
+class Translate extends BaseClient
 {
+
+    /**
+     * @var
+     */
+    public $appId;
+
+    public $appKey;
+
+    /**
+     * 指定百度翻译的API网址
+     */
+    public $apiBaseUrl = "http://api.fanyi.baidu.com/";
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+        if (empty ($this->appId)) {
+            throw new InvalidConfigException ('The "appId" property must be set.');
+        }
+        if (empty ($this->appKey)) {
+            throw new InvalidConfigException ('The "appKey" property must be set.');
+        }
+    }
 
     /**
      * 翻译
@@ -15,11 +46,21 @@ class Translate extends Baidu
      * @param string $to 目标语言语种：语言代码或auto
      * @return array
      */
-    public function get($query, $from = 'auto', $to = 'auto')
+    public function get($query, $from = 'auto', $to = 'zh')
     {
-        $query = urlencode($query);
-        $response = $this->api('apistore/tranlateservice/translate', 'GET', ['query' => $query, $from, $to]);
+        $response = $this->api('api/trans/vip/translate', 'POST', ['q' => $query, 'from' => $from, 'to' => $to]);
         return $response;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function apiInternal($url, $method, array $params, array $headers)
+    {
+        $salt = uniqid();
+        $sign = md5($this->appId . $params['q'] . $salt . $this->appKey);
+        $params = array_merge($params, ['appid' => $this->appId, 'sign' => $sign, 'salt' => $salt]);
+        return parent::apiInternal($url, $method, $params, $headers);
     }
 
     /**
