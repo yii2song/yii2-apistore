bb5b6b8053ca7f2b8d9e0b40688f2e3c7d9837c9
Xu Tongle <xutongle@gmail.com>
2016-02-01 10:38:28 +0800

    aaa

---
+++ tests/BaseClientTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace xutongle/extensions/apistore;
+
+use xutongle\apistore\BaseClient;
+
+class BaseClientTest extends TestCase
+{
+    public function testSetGet()
+    {
+        $client = new Client();
+
+        $id = 'test_id';
+        $client->setId($id);
+        $this->assertEquals($id, $client->getId(), 'Unable to setup id!');
+
+        $name = 'test_name';
+        $client->setName($name);
+        $this->assertEquals($name, $client->getName(), 'Unable to setup name!');
+
+    }
+
+    public function testGetDefaults()
+    {
+        $client = new Client();
+
+        $this->assertNotEmpty($client->getName(), 'Unable to get default name!');
+    }
+
+    
+}
+
+class Client extends BaseClient
+{
+}
